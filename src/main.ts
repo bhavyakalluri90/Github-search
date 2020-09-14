81f4437aa4b307d1582b3cf68ed0e430768ce35c
Bhavya Kalluri <71247450+bhavyakalluri90@users.noreply.github.com>
2020-09-14 00:35:53 -0500

    Add files via upload

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {MAT_FORM_FIELD_DEFAULT_OPTIONS} from '@angular/material/form-field';
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
